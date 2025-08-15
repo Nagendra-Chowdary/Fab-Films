579b74d9d3389e5b0447f84256121174fe139d15
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2025-08-15 20:10:22 +0530

    Added favourites section

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,9 @@
+
+export default function Favourite(){
+    return(
+        <div className="favourites-empty">
+        <h1>You have no favourites currently!</h1>
+        <p>Add your movies to favourites and they will appear here.</p>
+        </div>
+    );
+}
