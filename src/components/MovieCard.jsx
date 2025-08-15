cfa62871950a697e59b9efdc5bff82fbbbd3aa43
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2025-08-15 20:11:36 +0530

    Updated movie card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,26 @@
-export default function MovieCard({movie}){
-    function handleClick(){
-
-    }
-return(
+export default function MovieCard({ movie }) {
+  function handleClick() {}
+  return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} width={300} height={500}/>
+        <img src={movie.url} alt={movie.title} width={200} height={300} />
         <div className="movie-overlay">
-            <button className="favourite-btn" onClick={handleClick}>
-                <img src="/heart-angle-svgrepo-com.svg" alt="fav" width={50} height={50}/>
-                </button>
+          <button className="favourite-btn" onClick={handleClick}>
+            <img
+              src="/heart-angle-svgrepo-com.svg"
+              alt="fav"
+              width={20}
+              height={20}
+            />
+            {/* ❤️ */}
+          </button>
         </div>
+
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
       </div>
     </div>
-)
+  );
 }
