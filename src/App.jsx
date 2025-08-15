e2e8e9ec2c538af5064d2d2c71d241471c276370
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2025-08-15 20:13:18 +0530

    Routing changes and pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 // import { useState } from 'react'
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Favourites from './pages/Favourites'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import {Routes,Route} from 'react-router-dom'
 function App() {
-let movie={title:"movie",url:'Screenshot from 2024-05-23 19-48-44.png'}
+ 
+
   return (
-    <>
-    <MovieCard movie={movie} />
-   </>
+    <div className='top-div'>
+   <Navbar />
+    <main className='main-content'>
+
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path="/favourites" element={<Favourites />} />
+     </Routes>
+    </main>
+    </div>
   )
 }
 
